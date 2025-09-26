4cad4fe8cdd803a265e7705110e21f354883f1d8
raspi5-umt <umtcmrck@gmail.com>
2025-09-26 03:30:34 +0300

    Update Touch_1inch69.py

--- lib/Touch_1inch69.py
+++ lib/Touch_1inch69.py
@@ -1,5 +1,5 @@
 import time
-import config from Touch_1inch69 import Touch_1inch69
+from Touch_1inch69 import Touch_1inch69
 
 class Touch_1inch69(config.RaspberryPi):
 
@@ -74,6 +74,7 @@ class Touch_1inch69(config.RaspberryPi):
         
         self.X_point=x_point
         self.Y_point=y_point
+
 
 
 
